4a75abf645783bb6743c4cc1fe0af4d63909f6a3
Andrew Gilliland <andrewgilliland@Andrews-MacBook-Pro.local>
2025-03-17 21:50:37 -0500

    initial commit for lesson 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
-function Card() {
-  return (
-    <div>
-      <img src="https://picsum.photos/200"  />
-      <h2>Lorem, Ipsum.</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, necessitatibus veniam facilis optio rerum consequatur iure omnis velit. Optio, nobis!</p>
-    </div>
-  )
-}
+import Card from './components/Card'
 
 function App() {
   return (
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Card() {
+    return (
+      <div>
+        <img src="https://picsum.photos/200"  />
+        <h2>Lorem, Ipsum.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, necessitatibus veniam facilis optio rerum consequatur iure omnis velit. Optio, nobis!</p>
+      </div>
+    )
+}
+
+export default Card
