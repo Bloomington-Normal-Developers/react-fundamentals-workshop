68c2b3fb5703ab44e4fb0e311baa4a920022a61c
Andrew Gilliland <andrewgilliland@Andrews-MacBook-Pro.local>
2025-03-17 21:43:33 -0500

    initial commit for lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+function Card() {
+  return (
+    <div>
+      <img src="https://picsum.photos/200"  />
+      <h2>Lorem, Ipsum.</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, necessitatibus veniam facilis optio rerum consequatur iure omnis velit. Optio, nobis!</p>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <h1>React Fundamentals</h1>
+      <Card />
     </div>
   )
 }
